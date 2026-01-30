7bd7ce94fa71627a36e6b3c89a1f1cb82fb9325b
OnnaMcadva <anna.anutka@gmail.com>
2026-01-30 12:26:56 +0100

    zvezda

--- src/main/java/krpsim/optimizer/GreedyOptimizer.java
+++ src/main/java/krpsim/optimizer/GreedyOptimizer.java
@@ -4,7 +4,15 @@ import krpsim.model.Event;
 import krpsim.model.Process;
 import krpsim.utils.Parser;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
